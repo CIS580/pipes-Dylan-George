d74235152b1f514fb91ff126512b0a9d79d1473e
Dylan-George <dgeorge000@gmail.com>
2016-09-30 15:09:20 -0500

    Default

--- src/app.js
+++ src/app.js
@@ -7,49 +7,11 @@ const Game = require('./game');
 var canvas = document.getElementById('screen');
 var game = new Game(canvas, update, render);
 var image = new Image();
-image.src = 'assets/animals.png';
-//var blip = new Audio();
-//blip.src = 'assets/blip';
-//var flip = new Audio();
-//flip.src = 'assets/flip';
-//var pair = new Audio();
-//pair.src = 'assets/pair';
-
-// We have 9 pairs of possible cards that are about 212px square
-var cards = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
-var board = [];
-while(cards.length > 0) {
-  var index = Math.floor(Math.random() * (cards.length - 1));
-  board.push({card: cards[index], flip: true});
-  cards.splice(index, 1);
-}
-console.log(board);
-
-// TODO: Place the cards on the board in random order
+image.src = 'assets/pipes.png';
 
 canvas.onclick = function(event) {
   event.preventDefault();
-  switch(state)
-  {
-	case: "waiting for click";
-		if(!card[currentIndex])
-		{
-			blip.play;
-		}
-	break;
-  }
-  // TODO: determine which card was clicked on
-  // TODO: determine what to do
-}
-
-var currentIndex;
-var currentX;
-var currentY;
-canvas.onmousemove = function(event)
-{
-	event.preventDefault();
-	currentX = event.clientX/165;
-	currentY = event.clientY/165;
+  // TODO: Place or rotate pipe tile
 }
 
 /**
@@ -74,6 +36,7 @@ masterLoop(performance.now());
  */
 function update(elapsedTime) {
 
+  // TODO: Advance the fluid
 }
 
 /**
@@ -84,31 +47,9 @@ function update(elapsedTime) {
   * @param {CanvasRenderingContext2D} ctx the context to render to
   */
 function render(elapsedTime, ctx) {
-  ctx.fillStyle = "#ff7777";
+  ctx.fillStyle = "#777777";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   // TODO: Render the board
-  for(var y = 0; y < 3; y++) {
-    for(var x = 0; x < 6; x++) {
-      var card = board[y * 6 + x];
-      if(card.flip) {
-        // render cute animal
-        ctx.drawImage(image,
-          // Source rect
-          card.card % 3 * 212, Math.floor(card.card / 3) * 212, 212, 212,
-          // Dest rect
-          x * 165 + 3, y * 165 + 3, 160, 160
-        );
-      } else {
-        // draw the back of the card (212x212px)
-        ctx.fillStyle = "#3333ff";
-        ctx.fillRect(x * 165 + 3, y * 165 + 3, 160, 160);
-      }
-    }
-  }
-  
-	ctx.fillStyle = "red";
-	ctx.beginPath();
-	ctx.arc(currentX, currentY, 3, 0, 2*Math.PI);
-	ctx.fill();
+
 }
